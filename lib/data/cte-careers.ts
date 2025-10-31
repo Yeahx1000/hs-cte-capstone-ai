3895a2df84c22da985314be029d76fe58a69d1f6
Ekene <83151271+Yeahx1000@users.noreply.github.com>
2025-10-30 21:38:31 -0400

    CTE careers array creation, llm api route nested

---
+++ lib/data/cte-careers.ts
@@ -0,0 +1,20 @@
+// array of CTE careers, may use during chat prompt, or for suggestion cards. Also may change data type later.
+
+export const cteCareers = [
+    "Health Science",
+    "Business",
+    "Sales",
+    "Finance",
+    "Information Technology",
+    "Science, Technology, Engineering, and Mathematics (STEM)",
+    "Manufacturing",
+    "Logistics",
+    "Hospitality",
+    "Government",
+    "Law",
+    "Agriculture",
+    "Human Services",
+    "Construction",
+    "Training",
+    "Arts, Audio/Visual Technology, and Communications",
+];
